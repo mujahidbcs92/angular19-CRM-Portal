69a988f233bf36b662bf431c66f7b562e3999e3d
mujahidbcs92 <mujahidbcs92@gmail.com>
2025-05-11 23:29:09 +0400

    upgrade tailwind css and add menu component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MenuComponent } from './menu/menu.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MenuComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  standalone: true
 })
 export class AppComponent {
   title = 'CRM-Portal';
---
+++ src/app/menu/menu.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-menu',
+  imports: [],
+  templateUrl: './menu.component.html',
+  styleUrl: './menu.component.css',
+  standalone: true
+})
+export class MenuComponent {
+
+}
