67658817c1753350024ce3acc7a585fa536690f9
mujahidbcs92 <mujahidbcs92@gmail.com>
2025-05-11 23:33:14 +0400

    to get update tailwind latest -v

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+// import { RouterOutlet } from '@angular/router';
 import { MenuComponent } from './menu/menu.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MenuComponent],
+  imports: [MenuComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
   standalone: true
